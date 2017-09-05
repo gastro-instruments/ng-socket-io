96d8668d8f2349567c9ecdf6b0462fad4851b35a
Xue Qin <xq@gastro-instruments.de>
2017-09-05 13:56:01 +0200

    chore: adjust file name in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { SocketIoModule } from './socket-io.module';
-export { SocketIoConfig } from './socketIoConfig';
+export { SocketIoConfig } from './socket-io-config';
 export { WrappedSocket as Socket } from './socket-io.service';
